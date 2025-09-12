caa0c3ffe1eccf7902323c248943036550100f54
Carlos de Paula <me@carlosedp.com>
2025-09-12 19:37:10 -0300

    Fix to the epomaker/tide49 after raw_hid PR

--- keyboards/epomaker/tide49/linker/wireless/md_raw.h
+++ keyboards/epomaker/tide49/linker/wireless/md_raw.h
@@ -9,6 +9,7 @@
 #define _temp_rhs_29 replaced_hid_send  // raw_hid.h
 #define _temp_rhs_461 replaced_hid_send // via.c
 #define _temp_rhs_465 replaced_hid_send // via.c (current location)
+#define _temp_rhs_472 replaced_hid_send // via.c (new line number after PR #25255)
 #define _temp_rhs_609 replaced_hid_send // via.c (updated location)
 #define _temp_rhs_610 replaced_hid_send // via.c (new location)
 #define _temp_rhs_618 replaced_hid_send // via.c (newest location)
